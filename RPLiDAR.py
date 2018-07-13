4cbee97ed0ee301a3a88cb977617d74f32c21ed6
Jack J Amend <jamend@elon.edu>
2018-07-13 15:17:05 -0500

    Working on fixing readability and adding functions

--- RPLiDAR.py
+++ RPLiDAR.py
@@ -15,6 +15,9 @@ class RPLiDAR:
 
     def scan_area(self, limit=100):
         for i, scan in enumerate(self.lidar.iter_scans()):
+            if i > limit:
+                break
+
             print('%d: Got %d measurements' % (i, len(scan)))
             sector_space = np.zeros(6)
             warning_flag = False
@@ -23,23 +26,57 @@ class RPLiDAR:
                 if j[2] < 1000:
                     # print("Warning: object at %f degrees is too close"
                     # % (j[1]))
-                    sector_space[k] += -math.inf
+                    sector_space[k] += -math.inf # Set to say space is unsafe
                     warning_flag = True
                 else:
-                    sector_space[k] += j[2]
-                if math.floor(j[1])%60 == 0:
-                    k += 1
+                    sector_space[k] += j[2] # adding distance to sector array
 
             if warning_flag:
-                print(sector_space)
+                print(sector_space) # outputs six values
                 print("Object(s) are too close...")
                 free_space = max(sector_space)
                 if free_space < 1000:
                     print("There is nowhere safe to venture, immediate landing"
                           " to avoid damage...")
                     break
-                evacuation_direction = (sector_space.index(free_space)+1)*60 - 30
-                print("Evacuate in the direction of %d degrees" % evacuation_direction)
+                evacuation_direction = \
+                    (sector_space.index(free_space)+1) * 60 - 30
+                print("Evacuate in the direction of %d degrees"
+                      % evacuation_direction) # This is the safest spot
+
+
+    def area_report(self, limit=100, sectors=6):
+        for i, scans in enumerate(self.lidar.iter_scans()):
+            sector_space = np.zeros(sectors)
+            warning_flag = False
+
+            divisor = 360 / sectors
+
+            for scan in scans:
+                section = math.floor(scan[1] / divisor)
+                if scan[2] < 1000:
+                    # print("Warning: object at %f degrees is too close"
+                    # % (j[1]))
+                    sector_space[section] += -math.inf
+                    warning_flag = True
+                else:
+                    sector_space[section] += scan[2]
+                if math.floor(scan[1]) % 60 == 0:
+                    section += 1
+
+            if warning_flag:
+                print(sector_space)
+                print("Object(s) are too close...")
+                free_space = max(sector_space)
+                if free_space < 1000:
+                    print(
+                        "There is nowhere safe to venture, immediate landing"
+                        " to avoid damage...")
+                    break
+                evacuation_direction = \
+                    (sector_space.index(free_space) + 1) * 60 - 30
+                print("Evacuate in the direction of %d degrees"
+                      % evacuation_direction)
 
             if i > limit:
                 break
