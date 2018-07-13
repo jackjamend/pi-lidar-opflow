815fd2c02080d82d09d6af4c2a2607a3c15df5ff
Jack J Amend <jamend@elon.edu>
2018-07-13 15:02:53 -0500

    Updates to class

--- RPLiDAR.py
+++ RPLiDAR.py
@@ -6,7 +6,7 @@ import numpy as np
 
 class RPLiDAR:
     def __init__(self):
-        self.lidar = RPLidar("\\\\.\\com4") #Check to see if this runs on mac
+        self.lidar = RPLidar("\\\\.\\com4") # Check to see if this runs on mac
         #  laptop. If not make change. May be the /dev/ thing
         time.sleep(1)
         info = self.lidar.get_info()
@@ -16,30 +16,30 @@ class RPLiDAR:
     def scan_area(self, limit=100):
         for i, scan in enumerate(self.lidar.iter_scans()):
             print('%d: Got %d measurements' % (i, len(scan)))
-            ar = [0]*6
+            sector_space = np.zeros(6)
             warning_flag = False
             for j in scan:
                 k = math.floor(j[1]/60)
-                if j[2]<1000:
+                if j[2] < 1000:
                     # print("Warning: object at %f degrees is too close"
                     # % (j[1]))
-                    ar[k] += -math.inf
+                    sector_space[k] += -math.inf
                     warning_flag = True
                 else:
-                    ar[k] += j[2]
+                    sector_space[k] += j[2]
                 if math.floor(j[1])%60 == 0:
                     k += 1
 
             if warning_flag:
-                print(ar)
+                print(sector_space)
                 print("Object(s) are too close...")
-                fre = max(ar)
-                if fre<1000:
+                free_space = max(sector_space)
+                if free_space < 1000:
                     print("There is nowhere safe to venture, immediate landing"
                           " to avoid damage...")
                     break
-                evac = (ar.index(fre)+1)*60 - 30
-                print("Evacuate in the direction of %d degrees" % evac)
+                evacuation_direction = (sector_space.index(free_space)+1)*60 - 30
+                print("Evacuate in the direction of %d degrees" % evacuation_direction)
 
             if i > limit:
                 break
